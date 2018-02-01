2998c6110d974ae758061884d27bc7ffb635bdd4
rajsite <rajsite@users.noreply.github.com>
2018-01-31 22:02:10 -0600

    Create WebVIPolyfillRegistry class

---
+++ src/WebVIPolyfillRegistry.js
@@ -0,0 +1,13 @@
+
+class WebVIPolyfillRegistry {
+    constructor() {
+        this.polyfills = [];
+    }
+
+    // Expects an array of polyfill objects or a promise that resolves to an array of polyfill objects
+    define(polyfillOrPromise) {
+
+    }
+}
+
+export default WebVIPolyfillRegistry;
--- src/webvipolyfill.js
+++ src/webvipolyfill.js
@@ -1,8 +1,15 @@
 
 import xhookImport from 'xhook';
+import WebVIPolyfillRegistry from './WebVIPolyfillRegistry.js';
+
+// The current protocol version
+const protocolVersion = 1;
+
+// There are no valid http status codes >=600 so we abuse this range to encode webvipolyfill protocol version
+const protocolVersionAsStatusCode = protocolVersion + 600;
 
 // Just in-case, should update when this is resolved https://github.com/jpillora/xhook/issues/85
-var xhook;
+let xhook;
 if (xhookImport.enable === undefined) {
     if (xhookImport.xhook === undefined || xhookImport.xhook.enable === undefined) {
         throw new Error('Unrecognized xhook exports');
@@ -13,5 +20,17 @@ if (xhookImport.enable === undefined) {
     xhook = xhookImport;
 }
 
+// Synchronously set-up xhook to listen for polyfill network requests
+
+// Intercept HTTP Post requests
+// URL to intercept is webvipolyfill:<name>
+// where webvipolyfill: is a custom scheme and <name> is a string that corresponds to the defined polyfill
+
+// Example usage:
+// Load webvipolyfill.dist.js as the very first script
+
+
+
 export default {
+    WebVIPolyfillRegistry
 }
