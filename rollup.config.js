f506e5c0b9044e8307844ab6c12cb17d70373565
rajsite <rajsite@users.noreply.github.com>
2018-02-19 22:46:16 -0600

    Rename js source folder

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-    input: 'src/webvipolyfill.js',
+    input: 'js/webvipolyfill.js',
     output: {
         file: 'dist/webvipolyfill.concat.js',
         format: 'umd',
